e4a2980f29a12076a94684e1ce1ea3d5a7a49ba3
Gilson Miranda Junior <gilson.miranda@usp.br>
2020-10-26 21:08:00 -0300

    add

--- index.php
+++ index.php
@@ -1,37 +1,13 @@
 <?php
-//for loops
-$ninjas = ['shaun', 'ryu', 'yoshi'];
+//comparisons booleans (true or false)
 
-// for ($i = 0; $i < count($ninjas); $i++) {
-//     echo $ninjas[$i] . '<br/>';
-// };
+//loose vs strict equal comparison
 
-// foreach($ninjas as $ninja) {
-//     echo $ninja . '<br/>';
-// }
-
-$products = [
-    ['name' => 'shiny star', 'price' => 20],
-    ['name' => 'green shell', 'price' => 10],
-    ['name' => 'red shell', 'price' => 15],
-    ['name' => 'gold coin', 'price' => 5],
-    ['name' => 'lightning bolt', 'price' => 40],
-    ['name' => 'banana skin', 'price' => 2]
-];
-
-// foreach($products as $product) {
-//     echo $product ['name'] . ' - ' . $product['price'] . '<br/>';
-// }
+//numbers
 
 
-//while loop
 
-$i = 0;
-
-while($i < count($products)) {
-    echo $products[$i]['name'];
-    echo '<br />';
-    $i++;
-}
+//echo 5 < 10;
+//echo 4 > 10;
 
 ?>
